85b6505cde1175a09bcd8ade55c2b8277ec006bc
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2026-06-24 22:55:32 +0700

    v2.2

--- README.md
+++ README.md
@@ -8,14 +8,61 @@
 - Equalizer sound effects ported from Samsung Galaxy A53 5G (a53x) and integrated as a Magisk Module for all supported and rooted devices with Magisk
 - Global type sound effect
 - This is also allowed to install in One UI/TouchWiz ROM
-- MySpace, MySound, & SoundBooster effect can only be activated via post process stream mode (READ Optionals bellow!)
+- MySpace, MySound, & SoundBooster effects can only be activated via post process stream mode (READ Optionals bellow!)
 
 ## Sources
 - https://github.com/ItsLynix/samsung_a53x_dump a53xnaxx-user-13-TP1A.220624.014-A536BXXU4BVJG-release-keys
-- libmagiskpolicy.so: Kitsune Mask R6687BB53
+- libmagiskpolicy.so: Magisk (stable) 30.7 (30700)
+
+## Changelog
+
+v2.2
+- Support NoMount metamodule
+- Update libmagiskpolicy.so from Magisk (stable) 30.7 (30700)
+- Resets module folders/files permissions at post-fs-data
+- Move _uninstall.log to /data/adb/logs/
+- Removes conflicted weird modules
+- Does not disable raw playback (You can use Audio Compatibility Patch Reborn Magisk Module instead)
+
+v2.1
+- Fix wrong target in latest KernelSU
+
+v2.0
+- Tidy up aml.sh
+- Exclude \*audio\*effects\*haptic\*.xml
+- Abort installation if fail to mount mirror system
+- Fix wrong file permissions in some ROMs
+- Remove useless c2 codec service
+
+v1.9
+- Improve /odm and /my_product support detection
+
+v1.8
+- Fix some methods and crashes
+
+v1.7
+- Add Action button to clear apps caches
+- Fix architecture detection in some weird ROMs
+- Fix bug in uninstall.sh
+
+v1.6
+- Fix semDesktopModeEnabled method
+- DolbyManager.apk and XiaomyDolby.apk detection
+
+v1.5
+- Allow installation in Android Emulator
+
+v1.4
+- Improve \*audio\*effects\*.xml patch detection
+- Disable spatializer soundfx by default
+- Detects spatializer support if sa.spatial=1
+
+v1.3
+- Improve audio_effects.xml patch detection
+- Does not use media_codecs_c2_dolby_audio.xml if it's already exist
 
 ## Screenshots
-- https://t.me/androidryukimods/854
+https://t.me/androidryukimods/854
 
 ## Requirements
 - arm64-v8a or armeabi-v7a architecture
@@ -25,9 +72,9 @@
 - One UI Core Magisk Module installed in non-One UI/non-TouchWiz ROM https://github.com/reiryuki/One-UI-Core-Magisk-Module
 
 ## Installation Guide & Download Link
-- If you are using KernelSU, you need to disable Unmount Modules by Default in KernelSU app settings and install https://github.com/KernelSU-Modules-Repo/meta-overlayfs first
+- If you are using KernelSU, you need to disable Unmount Modules by Default in KernelSU app settings and install https://github.com/KernelSU-Modules-Repo/meta-overlayfs or https://github.com/KernelSU-Modules-Repo/magic_mount_rs or https://github.com/KernelSU-Modules-Repo/hybrid_mount or https://github.com/maxsteeel/nomount first depending on ROM compatibility
 - Install One UI Core Magisk Module first if you are in non-One UI/non-TouchWiz ROM: https://github.com/reiryuki/One-UI-Core-Magisk-Module
-- Install this module https://www.pling.com/p/1968026/ via Magisk app or Kitsune Mask app or KernelSU app or Apatch app or Recovery if Magisk or Kitsune Mask installed
+- Install this module via Magisk app or Kitsune Mask app or KernelSU app or Apatch app or Recovery if Magisk or Kitsune Mask installed
 - Install AML Magisk Module https://t.me/ryukinotes/34 only if using any other else audio mod module
 - Reboot
 - If you are using KernelSU, you need to allow superuser list manually all package name listed in package.txt (and your home launcher app also) (enable show system apps) and reboot afterwards
@@ -45,7 +92,7 @@
 - Stream: https://t.me/ryukinotes/52
 
 ## Troubleshootings
-- Global: https://t.me/ryukinotes/34
+Global: https://t.me/ryukinotes/34
 
 ## Support & Bug Report
 - https://t.me/ryukinotes/54
@@ -58,6 +105,6 @@
 - You can contribute ideas about this Magisk Module here: https://t.me/androidappsportdevelopment
 
 ## Sponsors
-- https://t.me/ryukinotes/25
+https://t.me/ryukinotes/25
 
 
--- service.sh
+++ service.sh
@@ -11,6 +11,27 @@ if [ ! -d $MODPATH/vendor ]\
 || [ -L $MODPATH/vendor ]; then
   MODSYSTEM=/system
 fi
+MOD=/data/adb/modules/nomount
+NM=$MOD/bin/nm
+NOMOUNT=false
+[ ! -f $MOD/disable ] && [ -x $NM ] && $NM v >/dev/null 2>&1 && NOMOUNT=true
+AML=/data/adb/modules/aml
+AUD=`cat $MODPATH/audio.txt`
+
+# NoMount
+if $NOMOUNT; then
+  if [ ! -d $AML ] || [ -f $AML/disable ]; then
+    FILES=`find $MODPATH/system $MODPATH/vendor -type f -name $AUD -o -name floating_feature.xml`
+    for FILE in $FILES; do
+      DES=`echo $FILE | sed -e "s|$MODPATH||g" -e 's|/system/odm|/odm|g' -e 's|/system/my_product|/my_product|g'`
+      RDES=`realpath $DES`
+      if [ -f $RDES ]; then
+        $NM del $RDES 2>/dev/null || true
+        $NM add $RDES $FILE
+      fi
+    done
+  fi
+fi
 
 # property
 resetprop -n ro.audio.ignore_effects false
@@ -28,43 +49,6 @@ fi
 killall $SERVER\
  android.hardware.audio@4.0-service-mediatek\
  android.hardware.audio.service
-
-# wait
-sleep 20
-
-# aml fix
-AML=/data/adb/modules/aml
-DIR=$AML$MODSYSTEM/vendor/odm/etc
-if [ "$API" -ge 26 ] && [ -d $DIR ]\
-&& [ ! -f $AML/disable ]; then
-  chcon -R u:object_r:vendor_configs_file:s0 $DIR
-fi
-AUD=`grep AUD= $MODPATH/copy.sh | sed -e 's|AUD=||g' -e 's|"||g'`
-DIR=$AML$MODSYSTEM/vendor
-FILES=`find $DIR -type f -name $AUD`
-if [ -d $AML ] && [ ! -f $AML/disable ]\
-&& find $DIR -type f -name $AUD; then
-  if ! grep '/odm' $AML/post-fs-data.sh && [ -d /odm ]\
-  && [ "`realpath /odm/etc`" == /odm/etc ]; then
-    for FILE in $FILES; do
-      DES=/odm`echo $FILE | sed "s|$DIR||g"`
-      if [ -f $DES ]; then
-        umount $DES
-        mount -o bind $FILE $DES
-      fi
-    done
-  fi
-  if ! grep '/my_product' $AML/post-fs-data.sh\
-  && [ -d /my_product ]; then
-    for FILE in $FILES; do
-      DES=/my_product`echo $FILE | sed "s|$DIR||g"`
-      if [ -f $DES ]; then
-        umount $DES
-        mount -o bind $FILE $DES
-      fi
-    done
-  fi
-fi
 
 # wait
 until [ "`getprop sys.boot_completed`" == 1 ]; do
@@ -124,7 +108,7 @@ if [ "$API" -ge 34 ]; then
   appops set $PKG READ_MEDIA_VISUAL_USER_SELECTED allow
 fi
 PKGOPS=`appops get $PKG`
-UID=`dumpsys package $PKG 2>/dev/null | grep -m 1 Id= | sed -e 's|    userId=||g' -e 's|    appId=||g'`
+UID=`grep "^$PKG " /data/system/packages.list | awk '{print $2}'`
 if [ "$UID" ] && [ "$UID" -gt 9999 ]; then
   appops set --uid "$UID" LEGACY_STORAGE allow
   appops set --uid "$UID" READ_EXTERNAL_STORAGE allow
@@ -141,7 +125,7 @@ fi
 
 # grant
 PKG=com.sec.android.app.soundalive
-if appops get $PKG > /dev/null 2>&1; then
+if appops get $PKG >/dev/null 2>&1; then
   pm grant --all-permissions $PKG
   appops set $PKG WRITE_SETTINGS allow
   if [ "$API" -ge 30 ]; then
@@ -151,7 +135,7 @@ if appops get $PKG > /dev/null 2>&1; then
     appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
   fi
   PKGOPS=`appops get $PKG`
-  UID=`dumpsys package $PKG 2>/dev/null | grep -m 1 Id= | sed -e 's|    userId=||g' -e 's|    appId=||g'`
+  UID=`grep "^$PKG " /data/system/packages.list | awk '{print $2}'`
   if [ "$UID" ] && [ "$UID" -gt 9999 ]; then
     UIDOPS=`appops get --uid "$UID"`
   fi
@@ -159,7 +143,7 @@ fi
 
 # grant
 PKG=com.samsung.android.soundassistant
-if appops get $PKG > /dev/null 2>&1; then
+if appops get $PKG >/dev/null 2>&1; then
   pm grant --all-permissions $PKG
   appops set $PKG SYSTEM_ALERT_WINDOW allow
   appops_set
@@ -167,14 +151,14 @@ fi
 
 # grant
 PKG=com.sec.hearingadjust
-if appops get $PKG > /dev/null 2>&1; then
+if appops get $PKG >/dev/null 2>&1; then
   pm grant --all-permissions $PKG
   appops set $PKG WRITE_SETTINGS allow
   if [ "$API" -ge 30 ]; then
     appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
   fi
   PKGOPS=`appops get $PKG`
-  UID=`dumpsys package $PKG 2>/dev/null | grep -m 1 Id= | sed -e 's|    userId=||g' -e 's|    appId=||g'`
+  UID=`grep "^$PKG " /data/system/packages.list | awk '{print $2}'`
   if [ "$UID" ] && [ "$UID" -gt 9999 ]; then
     UIDOPS=`appops get --uid "$UID"`
   fi
@@ -182,14 +166,14 @@ fi
 
 # grant
 PKG=com.samsung.android.setting.multisound
-if appops get $PKG > /dev/null 2>&1; then
+if appops get $PKG >/dev/null 2>&1; then
   pm grant --all-permissions $PKG
   appops set $PKG WRITE_SETTINGS allow
   if [ "$API" -ge 30 ]; then
     appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
   fi
   PKGOPS=`appops get $PKG`
-  UID=`dumpsys package $PKG 2>/dev/null | grep -m 1 Id= | sed -e 's|    userId=||g' -e 's|    appId=||g'`
+  UID=`grep "^$PKG " /data/system/packages.list | awk '{print $2}'`
   if [ "$UID" ] && [ "$UID" -gt 9999 ]; then
     UIDOPS=`appops get --uid "$UID"`
   fi
